26d1d413ad15ce8efedefc7772b7d049d1426eb1
Booo\Boo <anggarasaja98@gmail.com>
2018-05-23 14:08:30 +0700

    no message

--- application/models/ModelLogin.php
+++ application/models/ModelLogin.php
@@ -7,11 +7,21 @@ class ModelLogin extends CI_Model{
 		$this ->load ->database();
 	}
 
-	public function cek_dosen($user, $pass){
+	public function cek_dosen($user){
 
 		// return $this ->db ->query("select count(*) from tmst_user where tmst_dosen_nip='$user'
 		// ");
-		$sql = "select count(*) from tmst_user where tmst_dosen_nip='$user'";
+		$sql = "select count(tmst_dosen_nip) from tmst_user where tmst_dosen_nip='$user'";
+		$query = $this ->db ->query($sql);
+		return $row = $query ->row_array();
+		// return $row['tmst_dosen_nip'] ==1;
+	}
+
+	public function cek_mhs($user){
+
+		// return $this ->db ->query("select count(*) from tmst_user where tmst_dosen_nip='$user'
+		// ");
+		$sql = "select count(tmst_mahasiswa_nim) from tmst_user where tmst_mahasiswa_nim='$user'";
 		$query = $this ->db ->query($sql);
 		return $row = $query ->row_array();
 		// return $row['tmst_dosen_nip'] ==1;
@@ -28,11 +38,33 @@ class ModelLogin extends CI_Model{
 		return $query;
 	}
 
+	public function get_pass_mhs($user){
+
+		$this ->db ->select('*');
+		$this ->db ->from('tmst_user');
+		$this ->db ->where('tmst_mahasiswa_nim',$user);
+
+		$query = $this ->db ->get();
+
+		return $query;
+	}
+
 	public function get_level_dosen($user){
 
 		$this ->db ->select('*');
 		$this ->db ->from('tmst_user');
 		$this ->db ->where('tmst_dosen_nip',$user);
+
+		$query = $this ->db ->get();
+
+		return $query;
+	}
+
+	public function get_level_mhs($user){
+
+		$this ->db ->select('*');
+		$this ->db ->from('tmst_user');
+		$this ->db ->where('tmst_mahasiswa_nim',$user);
 
 		$query = $this ->db ->get();
 
