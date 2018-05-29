39a21640d5102e9fc5e75ec3e3253e5c20a86307
Booo\Boo <anggarasaja98@gmail.com>
2018-05-29 21:58:10 +0700

    no message

--- application/views/MenuAdmin.php
+++ application/views/MenuAdmin.php
@@ -9,32 +9,43 @@
                                     <li><a href="dashboard-2.html">Dashboard 2</a></li>
                                 </ul>
                             </li> -->
-                            <li>
-                                <a href="<?php echo base_url(); ?>home"><i class="fa fa-home"></i><span> Beranda</span></a>
+                            <?php if($this ->session ->userdata('level')==1){?>
+                                <li>
+                                    <a href="<?php echo base_url(); ?>koordinator"><i class="fa fa-home"></i><span> Beranda</span></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-list"></i> <span> List Mahasiswa</span></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-list"></i> <span> Bimbingan</span></a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url('koordinator/privileges')?>"><i class="fa fa-lock"></i> <span> Privileges</span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript: void(0);" aria-expanded="true"><i class="fa fa-lock"></i> <span> Grafik<span> <span class="menu-arrow"></span></a>
+                                    <ul class="nav-second-level nav" aria-expanded="true">
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                        <li><a href="#"> Grafik Total Usulan</a></li>
+                                    </ul>
+                                </li>
+                            <?php }else if($this ->session ->userdata('level')==2){?>
+                                <li>
+                                    <a href="<?php echo base_url(); ?>reviewer"><i class="fa fa-home"></i><span> Beranda2</span></a>
+                                </li>
+                            <?php }else if($this ->session ->userdata('level')==3){?>
+                                <li>
+                                    <a href="<?php echo base_url(); ?>dosen"><i class="fa fa-home"></i><span> Beranda3</span></a>
+                                </li>
+                            <?php }else{ ?>
+                                <li>
+                                <a href="<?php echo base_url(); ?>datamaster"><i class="fa fa-home"></i><span> Beranda0</span></a>
                             </li>
-                            <li>
-                                <a href="#"><i class="fa fa-list"></i> <span> List Mahasiswa</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-list"></i> <span> Bimbingan</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-list"></i> <span> Review Judul</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-lock"></i> <span> Privileges</span></a>
-                            </li>
-                            <li>
-                                <a href="javascript: void(0);" aria-expanded="true"><i class="fa fa-lock"></i> <span> Grafik<span> <span class="menu-arrow"></span></a>
-                                <ul class="nav-second-level nav" aria-expanded="true">
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                    <li><a href="#"> Grafik Total Usulan</a></li>
-                                </ul>
-                            </li>
+                            <?php }?>
                             <!-- <li>
                                 <a href="javascript: void(0);" aria-expanded="true"><i class="fa fa-lock"></i> <span> Privileges<span> <span class="menu-arrow"></span></a>
                                 <ul class="nav-second-level nav" aria-expanded="true">
