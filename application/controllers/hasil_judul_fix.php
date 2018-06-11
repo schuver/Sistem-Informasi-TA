1d350ef407448de8c2c5af7419f7c20bac34d5df
RERA-PC\RERA <marerapm82@gmail.com>
2018-06-11 10:38:57 +0700

    hasil_judul_fix FIX

--- application/controllers/hasil_judul_fix.php
+++ application/controllers/hasil_judul_fix.php
@@ -9,7 +9,10 @@ class hasil_judul_fix extends CI_Controller{
 	
 	function index(){
 	$data = array(
-		'data'=>$this->m_hasil_judul_fix->get_data());
+		'data'=>$this->m_hasil_judul_fix->get_data(),
+		"menu" => "MenuMhs",
+                 "panelbody" => "mahasiswa/index");
+				 $this->load->view('panelbody', $data);
 		$this->load->view('hasil_judul_fix/v_hasil_judul_fix',$data);
 	}
 	}
