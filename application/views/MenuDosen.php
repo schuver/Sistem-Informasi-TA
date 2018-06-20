959711c2df21650ae90825741593f438cfb47061
user-PC\Afega <avegawira@gmail.com>
2018-06-20 10:59:00 +0700

    no message

---
+++ application/views/MenuDosen.php
@@ -0,0 +1,40 @@
+ 
+                        <!-- Left Menu Start -->
+                        <ul class="metisMenu nav" id="side-menu"> 
+                            <li class="menu-title">List Menu</li>
+                            <!-- <li>
+                                <a href="javascript: void(0);" aria-expanded="true"><i class="fi-air-play"></i><span class="badge badge-success pull-right">2</span> <span> Dashboard </span></a>
+                                <ul class="nav-second-level nav" aria-expanded="true">
+                                    <li><a href="index.html">Dashboard 1</a></li>
+                                    <li><a href="dashboard-2.html">Dashboard 2</a></li>
+                                </ul>
+                            </li> -->
+                            <li> 
+                                <a href="<?php echo base_url(); ?>dosen"><i class="fa fa-home"></i><span> Beranda</span></a> 
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url('dosen/list_mhs')?>"><i class="fa fa-list"></i> <span> List Mahasiswa</span></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-list"></i> <span> Bimbingan</span></a>
+                            </li>
+                            <!-- <li>
+                                <a href="javascript: void(0);" aria-expanded="true"><i class="fa fa-lock"></i> <span> Privileges<span> <span class="menu-arrow"></span></a>
+                                <ul class="nav-second-level nav" aria-expanded="true">
+                                    <li><a href="admin-grid.html">Grid</a></li>
+                                    <li><a href="admin-sweet-alert.html">Sweet Alert</a></li>
+                                    <li><a href="admin-tiles.html">Tiles Box</a></li>
+                                    <li><a href="admin-nestable.html">Nestable List</a></li>
+                                    <li><a href="admin-rangeslider.html">Range Slider</a></li>
+                                    <li><a href="admin-ratings.html">Ratings</a></li>
+                                    <li><a href="admin-filemanager.html">File Manager</a></li>
+                                    <li><a href="admin-lightbox.html">Lightbox</a></li>
+                                    <li><a href="admin-scrollbar.html">Scroll bar</a></li>
+                                    <li><a href="admin-slider.html">Slider</a></li>
+                                    <li><a href="admin-treeview.html">Treeview</a></li>
+                                </ul>
+                            </li> -->
+
+                        </ul>
+
+                    
